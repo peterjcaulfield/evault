5139dcf50d26b1b9329da1fd01cde2cdeb0c733e
Peter Caulfield <peterjcaulfield@gmail.com>
2013-12-02 16:48:34 +0000

    first commit

---
+++ e-vault/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  e-vault
+//
+//  Created by Peter Caulfield on 02/12/2013.
+//  Copyright (c) 2013 Peter Caulfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ e-vaultTests/e_vaultTests.m
@@ -0,0 +1,34 @@
+//
+//  e_vaultTests.m
+//  e-vaultTests
+//
+//  Created by Peter Caulfield on 02/12/2013.
+//  Copyright (c) 2013 Peter Caulfield. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface e_vaultTests : XCTestCase
+
+@end
+
+@implementation e_vaultTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
